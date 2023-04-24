c73d0da487cc6303ac8d4847ed9d011af0f6ec06
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-04-24 18:51:15 -0300

    files cleaned

---
+++ src/Components/Content.jsx
@@ -0,0 +1,12 @@
+import '../Styles/Content.css';
+import React from 'react';
+
+class Content extends React.Component{
+    render(){
+        return(
+            <h1>Test Content</h1>
+        )
+    }
+}
+
+export default Content;
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,12 @@
+import '../Styles/NavBar.css';
+import React from 'react';
+
+class Navbar extends React.Component{
+    render(){
+        return(
+            <h1>Test Navbar</h1>
+        )
+    }
+}
+
+export default Navbar;
