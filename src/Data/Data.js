2b21195ede616dec5a7870ff2970f76e950aee73
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-04-24 19:04:20 -0300

    adding images and Data the the files

---
+++ src/Data/Data.js
@@ -0,0 +1,30 @@
+export default [
+    {
+      title: "Mount Fuji",
+      location: "Japan",
+      googleMapsUrl: "https://www.google.com/maps/place/Mount+Fuji/@35.3606249,138.7222028,16z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA",
+      startDate: "24 Jan, 2021",
+      endDate: "24 Feb, 2021",
+      description: "Mount Fuji is the tallest mount of Japan",
+      imageUrl: "https://www.pexels.com/de-de/foto/natur-feld-berg-gras-16394365/"
+    },
+    {
+      title: "Sydney Opera House",
+      location: "Australia",
+      googleMapsUrl: "https://www.google.com/maps/place/Sydney+Opera+House/@-33.8567844,151.2127164,17z/data=!3m2!4b1!5s0x6b12ae67d234a27f:0xd6d4e9380ca1e32f!4m6!3m5!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!8m2!3d-33.8567844!4d151.2152967!16zL20vMDZfbm0",
+      startDate: "10 Apr, 2023",
+      endDate: "15 Apr, 2023",
+      description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney, Australia.",
+      imageUrl: "https://www.pexels.com/de-de/foto/opernhaus-in-sydney-2434267/"
+    },
+    {
+      title: "Geirangerfjord",
+      location: "Norway",
+      googleMapsUrl: "https://www.google.com/maps/place/Geirangerfjord/@62.1081535,6.8798736,11.28z/data=!4m6!3m5!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940817!16zL20vMDZ4eXFi",
+      startDate: "1 Jul, 2023",
+      endDate: "5 Jul, 2023",
+      description: "Geirangerfjord is a beautiful fjord in Norway known for its stunning scenery and waterfalls.",
+      imageUrl: "https://www.pexels.com/de-de/foto/landschaft-berge-natur-fluss-12671393/"
+    }
+  ]
+  
