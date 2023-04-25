da6c374c56bca21eb3d2ab0df7e22a45cdb4432a
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-04-24 21:17:57 -0300

    adding Data from Data.js to App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import Content from './Components/Content';
 import Navbar from './Components/NavBar';
 import './App.css';
+import contentData from './Data/Data';
 
 function App() {
-
+const contentElements = contentData.map(item =>{
+  return(
+    <Content 
+    key={item.id}
+    {...item}/>
+  )
+})
   return (
     <div className="App">
       <Navbar />
       <div className='content-app'>
-      <Content />     
-      <Content />      
-      < Content />
+      {contentElements}
       </div>
       
     </div>
